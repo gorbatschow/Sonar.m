bfb00637b6e989266790a4804fc5e78c64788dbf
gorbatschow <gorbatschow.r@gmail.com>
2022-12-19 06:14:10 +0300

    add marczak equation

---
+++ toolbox/sound_speed/sound_speed_water_marczak.m
@@ -0,0 +1,16 @@
+function C = sound_speed_water_marczak(T)
+% Arguments
+%    T: temperature \ degree Celsius \ 0 < T < 95
+% Results
+%    C: speed of sound in pure water \ m/s
+
+    k0 = +1.402385e+3;
+    k1 = +5.038813e+0;
+    k2 = -5.799136e-2;
+    k3 = +3.287156e-4;
+    k4 = -1.398845e-6;
+    k5 = +2.787860e-9;
+
+    C = k0*(T.^0) + k1*(T.^1) + k2*(T.^2) ...
+        + k3*(T.^3) + k4*(T.^4) + k5*(T.^5);   
+end
