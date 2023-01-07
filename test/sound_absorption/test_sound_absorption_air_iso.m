150c9410949d4ac478a7313dd6cdefa83b892ece
gorbatschow <gorbatschow.r@gmail.com>
2023-01-07 17:09:04 +0300

    work on iso air absorption equation

--- test/sound_absorption/test_sound_absorption_air_iso.m
+++ test/sound_absorption/test_sound_absorption_air_iso.m
@@ -1,7 +1,7 @@
 function test_sound_absorption_air_iso()
     
     test(1) = struct('T', 21, 'RH', 50, 'P', 101.325, 'f', 1000, ...
-        'alpha', 0.004829827884631664);
+        'alpha', 0.0048313242022907);
    
     sonarm_eval_test(@sound_absorption_air_iso, test);
 end
--- toolbox/sound_absorption/sound_absorption_air_iso_annex.m
+++ toolbox/sound_absorption/sound_absorption_air_iso_annex.m
@@ -1,4 +1,4 @@
-function alpha = sound_absorption_air_iso(T,RH,P,f)
+function alpha = sound_absorption_air_iso_annex(T,RH,P,f)
 % Arguments
 %   T: temperature \ degree Celsius \ -20 < T < +50
 %   RH: relative humidity \ percentage \ 10 < RH < 100
@@ -6,7 +6,7 @@ function alpha = sound_absorption_air_iso(T,RH,P,f)
 %   f: frequency \ kHz \ 0.05 < f < 50
 % Results
 %   alpha: absorption of sound in air \ dB/m
-
+    
     Kelvin = 273.15;
     T_ref = Kelvin + 20;
     T_kel = Kelvin + T;
@@ -15,20 +15,24 @@ function alpha = sound_absorption_air_iso(T,RH,P,f)
     Tho = 2239.1;
     Thn = 3352.0;
     P_ref = 101.325;
-    P_rel = P / P_ref;
+    P_rel = P / P_ref;    
     
     P_sat_P_ref = 10.^(-6.8346*(T_01./T_kel).^(1.261)+4.6151);
     H = RH.*(P_sat_P_ref./P_rel);
-    
+
     Fro = P_rel.*(24+(4.04e4)*H.*(0.02+H)./(0.391+H));
     Frn = P_rel.*(T_rel.^(-1/3)).*(9+280*H*exp(-4.170*(T_rel.^(-1/3)-1)));
 
-    Xc = (1.84e-11)./P_rel*(T_rel.^(1/2));
-    Xo = 0.01275*exp(-Tho./T_kel)./(Fro+((f.^2)./Fro));
-    Xn = 0.10680*exp(-Thn./T_kel)./(Frn+((f.^2)./Frn));
+    C = 343.2*(T_rel.^(1/2));
+    acr = (1.60e-10)*(T_rel.^(1/2)).*(f.^2)./P_rel;
+    
+    Xo = 0.209;
+    amaxO = 1.559*Xo*((Tho./T_kel).^2).*exp(-Tho./T_kel);
+    avibO = amaxO.*(f/C).*2*(f./Fro)./(1+(f./Fro).^2);
+    
+    Xn = 0.781;
+    amaxN = 1.559*Xn*((Thn./T_kel).^2).*exp(-Thn./T_kel);
+    avibN = amaxN.*(f/C).*2*(f./Frn)./(1+(f./Frn).^2);
 
-    alpha = 8.686*(f.^2).*(Xc+(T_rel.^(-5/2)).*(Xo+Xn));
+    alpha = acr + avibO + avibN;
 end
-
-% http://resource.npl.co.uk/acoustics/techguides/absorption/
-
